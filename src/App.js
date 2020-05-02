ace738921d79d47fe58b3c09f38ff611f118edb7
Martin Rancourt <vonziper@gmail.com>
2020-05-02 12:05:30 -0700

    edit points of sale

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Home from "./Home";
-import {Parallax, ParallaxBanner, ParallaxProvider} from 'react-scroll-parallax';
+import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhone, faMapMarkerAlt, faBreadSlice, faExchangeAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
@@ -41,13 +40,23 @@ function App() {
 
             <div className="columns">
               <div className="column col-lg-12">
+
+                <p className="location">
+                  Saint-Côme-Linière
+                </p>
+
+                <div className="point-of-sale">
+                  <p className="title">Les Marchés Tradition</p>
+                  <p className="address">940 Rue Principale</p>
+                </div>
+
                 <p className="location">
                   Saint-Georges
                 </p>
 
                 <div className="point-of-sale">
                   <p className="title">Dépanneur L'Essentiel</p>
-                  <p className="address">12755 36e Avenue</p>
+                  <p className="address">12755 36<sup>e</sup> Avenue</p>
                 </div>
 
                 <div className="point-of-sale">
@@ -62,17 +71,17 @@ function App() {
 
                 <div className="point-of-sale">
                   <p className="title">IGA families Rodrigue et Groleau</p>
-                  <p className="address">911 150e Rue</p>
+                  <p className="address">911 150<sup>e</sup> Rue</p>
                 </div>
 
                 <div className="point-of-sale">
                   <p className="title">Le Jardin des Sages</p>
-                  <p className="address">10435 1 Avenue</p>
+                  <p className="address">10435 1<sup>e</sup> Avenue</p>
                 </div>
 
                 <div className="point-of-sale">
                   <p className="title">Le Grand Marché</p>
-                  <p className="address">12120, 1e Avenue</p>
+                  <p className="address">12120, 1<sup>e</sup> Avenue</p>
                 </div>
 
                 <div className="point-of-sale">
@@ -84,19 +93,17 @@ function App() {
                   <p className="title">Metro Laval Veilleux</p>
                   <p className="address">17850 Boulevard Lacroix</p>
                 </div>
-              </div>
-              <div className="column col-lg-12">
 
                 <p className="location">
-                  Saint-Benoît-Labre
+                  Saint-Prosper
                 </p>
 
                 <div className="point-of-sale">
-                  <p className="title">Marché Saint-Benoît</p>
-                  <p className="address">130 Rue Principale</p>
+                  <p className="title">L'Inter-Marché</p>
+                  <p className="address">2600-2798 25<sup>e</sup> Avenue</p>
                 </div>
-
-
+              </div>
+              <div className="column col-lg-12">
                 <p className="location">
                   Beauceville
                 </p>
@@ -125,26 +132,21 @@ function App() {
                 </div>
 
                 <p className="location">
+                  Saint-Benoît-Labre
+                </p>
+
+                <div className="point-of-sale">
+                  <p className="title">Marché Saint-Benoît</p>
+                  <p className="address">130 Rue Principale</p>
+                </div>
+
+                <p className="location">
                   Saint-Joseph
                 </p>
 
                 <div className="point-of-sale">
                   <p className="title">IGA Marché Pierre Jobidon</p>
                   <p className="address">1021 Avenue du Palais #101</p>
-                </div>
-
-                <p className="location">
-                  Saint-Prosper
-                </p>
-
-                <div className="point-of-sale">
-                  <p className="title">Colette Fleuriste</p>
-                  <p className="address">2715, 20E Avenue</p>
-                </div>
-
-                <div className="point-of-sale">
-                  <p className="title">L'Inter-Marché</p>
-                  <p className="address">2600-2798 25e Avenue</p>
                 </div>
 
                 <p className="location">
@@ -155,7 +157,63 @@ function App() {
                   <p className="title">Les Marchés Tradition</p>
                   <p className="address">137 Rue Commerciale</p>
                 </div>
+
+                <p className="location">
+                  Saint-Zacharie
+                </p>
+
+                <div className="point-of-sale">
+                  <p className="title">Cado-Gaz inc.</p>
+                  <p className="address">713, 15<sup>e</sup> Rue</p>
+                </div>
               </div>
+            </div>
+          </div>
+        </section>
+
+        {/*<ParallaxBanner*/}
+          {/*layers={[*/}
+            {/*{*/}
+              {/*image: './images/pain6.jpg',*/}
+              {/*amount: .5,*/}
+            {/*}*/}
+          {/*]}*/}
+          {/*style={{*/}
+            {/*height: '300px',*/}
+          {/*}}*/}
+        {/*/>*/}
+
+        <section id="points_de_vente" className="light">
+          <div className="container">
+            <h2 className="pos-title">
+              Restaurants
+            </h2>
+
+            <div className="columns">
+              <div className="column col-lg-12">
+
+                <p className="location">
+                  Saint-Georges
+                </p>
+
+                <div className="point-of-sale">
+                  <p className="title">VG Café</p>
+                  <p className="address">11780 1<sup>e</sup> Avenue</p>
+                </div>
+              </div>
+
+              <div className="column col-lg-12">
+
+                <p className="location">
+                  Saint-Côme-Linière
+                </p>
+
+                <div className="point-of-sale">
+                  <p className="title">Resto-Bar Le Brûlé</p>
+                  <p className="address">1327 Rue Principale</p>
+                </div>
+              </div>
+
             </div>
           </div>
         </section>
@@ -196,6 +254,9 @@ function App() {
                   1251 Route du Président-Kennedy
                   <br/>
                   Saint-Côme-Linière, QC G0M 1J0
+                  <br/>
+                  <FontAwesomeIcon icon={faEnvelope} />{' '}
+                  <a href="mailto:boulangeriedici@gmail.com">boulangeriedici@gmail.com</a>
                   <br/>
                   <FontAwesomeIcon icon={faPhone} />{' '}
                   <a href="tel:1-833-685-7246">1-833-685-7246</a>
